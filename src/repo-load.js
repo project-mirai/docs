86b4da839f40f4f9223d1bc88567ef67851aca02
Karlatemp <karlatemp@vip.qq.com>
2021-07-19 19:40:58 +0800

    Use absolute path for settings fetching

--- src/repo-load.js
+++ src/repo-load.js
@@ -1,8 +1,10 @@
 let navs = require('./nav');
 let config = require('./config');
+let path = require('path');
 
 module.exports = {
     onLoad: function (repoName, repoLoc, docRel) {
+        repoLoc = path.resolve(repoLoc);
         var docConf;
         if (docRel == undefined) {
             docConf = repoLoc;
@@ -11,8 +13,7 @@ module.exports = {
         }
         docConf += '/.conf';
         try {
-            let nv = require(docConf + '/nav');
-            navs[repoName] = nv;
+            navs[repoName] = require(docConf + '/nav');
         } catch (ignore) {
             if (config.verbose.show_debug) {
                 console.log(ignore)
